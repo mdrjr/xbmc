24d02296f02f3eab4d16f5bd211a5bc7649a95c6
Valentin Manea <valentin.manea@mrs.ro>
2013-10-21 21:58:27 +0000

    Fix some logic errors in the native window handling

--- xbmc/windowing/egl/EGLWrapper.cpp
+++ xbmc/windowing/egl/EGLWrapper.cpp
@@ -17,7 +17,7 @@
  *  <http://www.gnu.org/licenses/>.
  *
  */
-
+#include "EGLNativeTypeHybris.h"
 #include "system.h"
 
 #ifdef HAS_EGL
@@ -295,7 +295,7 @@ bool CEGLWrapper::CreateSurface(EGLDisplay display, EGLConfig config, EGLSurface
   EGLNativeWindowType *nativeWindow=NULL;
   if (!m_nativeTypes->GetNativeWindow((XBNativeWindowType**)&nativeWindow))
     return false;
-
+  
   *surface = eglCreateWindowSurface(display, config, *nativeWindow, NULL);
   CheckError();
   return *surface != EGL_NO_SURFACE;
@@ -401,7 +401,7 @@ void CEGLWrapper::SwapBuffers(EGLDisplay display, EGLSurface surface)
 
 #if defined(TARGET_HYBRIS)
   if (m_nativeTypes)
-    m_nativeTypes->ShowWindow(show);
+    m_nativeTypes->ShowWindow(true);
 #endif
 }
 
