b143c7b8bb4993391a3177d2c22ad87dae5522f2
memeka <mihailescu2m@gmail.com>
2013-11-16 22:35:00 +0000

    take out xbmc debug message; enable airplay in config flags

--- xbmc/windowing/egl/EGLNativeTypeHybris.cpp
+++ xbmc/windowing/egl/EGLNativeTypeHybris.cpp
@@ -51,7 +51,6 @@ void CHybrisVideoRenderer::Process()
 {
 //  m_clock = 0;
 //  m_lastTime = XbmcThreads::SystemClockMillis();
-  printf("We started!\n");
   while (!m_bStop)
   {
   HWComposerNativeWindow* window = (HWComposerNativeWindow*)m_hwNativeWindow;
@@ -295,9 +294,9 @@ bool CEGLNativeTypeHybris::GetNativeResolution(RESOLUTION_INFO *res) const
   res->fPixelRatio   = 1.0f;
   res->iScreenWidth  = res->iWidth;
   res->iScreenHeight = res->iHeight;
-  res->strMode.Format("%dx%d @ %.2f%s - Full Screen", res->iScreenWidth, res->iScreenHeight, res->fRefreshRate,
-  res->dwFlags & D3DPRESENTFLAG_INTERLACED ? "i" : "");
-  CLog::Log(LOGNOTICE,"Current resolution: %s\n",res->strMode.c_str());
+  //res->strMode.Format("%dx%d @ %.2f%s - Full Screen", res->iScreenWidth, res->iScreenHeight, res->fRefreshRate,
+  res->dwFlags & D3DPRESENTFLAG_INTERLACED ? "i" : "";
+  //CLog::Log(LOGNOTICE,"Current resolution: %s\n",res->strMode.c_str());
   return true;
 #endif
 }
